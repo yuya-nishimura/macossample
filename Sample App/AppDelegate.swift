5ad3a1be6a75c709fc427833f5e50987e1195a9b
Yuya Nishimura <y-nishimura@r-learning.co.jp>
2019-07-25 13:22:18 +0900

    Revert "Revert "initial commit: MacOS用アプリ作成てすと""
    This reverts commit c6e1c6b160d0c23f42c3fec0fe9dbb53e4097e87.

---
+++ Sample App/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sample App
+//
+//  Created by Yuya Nishimura on 2019/07/25.
+//  Copyright © 2019 Yuya Nishimura. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sample App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sample App
+//
+//  Created by Yuya Nishimura on 2019/07/25.
+//  Copyright © 2019 Yuya Nishimura. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
